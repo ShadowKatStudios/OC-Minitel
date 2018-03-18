cad7de5c38308de115cfdcaf0855c7148265f76a
Izaya <redtailedairways@gmail.com>
2018-03-18 13:48:13 +1100

    removed some unneccesary code from microtel-4

--- Embedded/microtel-4.lua
+++ Embedded/microtel-4.lua
@@ -1,15 +1,11 @@
 net.mtu = 4096
-function net.send(to,vport,ldata)
+function net.lsend(to,vport,ldata)
  local tdata = {}
- if ldata:len() > net.mtu then
-  for i = 1, ldata:len(), net.mtu do
-   tdata[#tdata+1] = ldata:sub(1,net.mtu)
-   ldata = ldata:sub(net.mtu+1)
-  end
- else
-  tdata = {ldata}
+ for i = 1, ldata:len(), net.mtu do
+  tdata[#tdata+1] = ldata:sub(1,net.mtu)
+  ldata = ldata:sub(net.mtu+1)
  end
  for k,v in ipairs(tdata) do
-  net.rsend(to,vport,v)
+  net.send(to,vport,v)
  end
 end
