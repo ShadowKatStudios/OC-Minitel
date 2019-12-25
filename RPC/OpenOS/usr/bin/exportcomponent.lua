f896547ea9ef9ebb4ceef54274ae3a0d580716b3
XeonSquared <redtailedairways@gmail.com>
2019-12-25 16:50:52 +1100

    added a usage text to exportcomponent

--- RPC/OpenOS/usr/bin/exportcomponent.lua
+++ RPC/OpenOS/usr/bin/exportcomponent.lua
@@ -2,6 +2,10 @@ local component = require "component"
 local rpc = require "rpc"
 local tA = {...}
 
+if #tA < 1 then
+ print("Usage: exportcomponent <component address> [component address...]")
+end
+
 for k,v in ipairs(tA) do
  local px = component.proxy(component.get(v))
  print(px.type.."_"..px.address)
