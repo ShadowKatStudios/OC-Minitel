e0895522bd7a73adacdf82e0b3e15d4c6321090c
Izzy <izzy@shadowkat.net>
2025-09-10 07:15:29 +1000

    fix mounting r/w shares

--- MTFS/OpenOS/usr/bin/importfs.lua
+++ MTFS/OpenOS/usr/bin/importfs.lua
@@ -57,10 +57,7 @@ if px.dirstat then -- use single call for file info
   return gce(path, 4) or olm(path)
  end
 end
-local iro,e = px.isReadOnly()
-if not iro then
- error(e)
-end
+local iro = px.isReadOnly()
 function px.isReadOnly()
  return iro
 end
